3d3fd38f7f28b8574285ed874382580ed6160471
william <daixiang1992@gmail.com>
2024-09-25 18:39:33 +0800

    feat: 简单调整主页样式

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,15 +1,17 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Hero() {
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Markdown Tree</h1>
-          <p className="py-6">一个用于构建交互式Markdown树状图的强大工具</p>
-          <Link href="/read" className="btn btn-primary">开始使用</Link>
+      <div className="hero-content text-center mt-[-12rem]">
+        <div className="max-w-xl">
+          <h1 className="text-6xl font-bold mb-10">高效的阅读和写作</h1>
+          <h2 className="text-5xl mb-10">清晰的结构 + 简洁的内容</h2>
+          <Link href="/read" className="btn btn-primary">
+            开始使用
+          </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
