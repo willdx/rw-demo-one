2f2411ea1f225b4c02d12ad11b8770ec5ed76a13
william <daixiang1992@gmail.com>
2024-10-10 13:54:17 +0800

    feat: tiny

--- app/share/page.tsx
+++ app/share/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_PUBLISHED_DOCUMENTS, SEARCH_DOCUMENTS } from "../graphql/queries";
 import Link from "next/link";
 import Header from "../components/Header";
 import Toast from "../components/Toast";
 import { useAuth } from "../contexts/AuthContext";
+import debounce from "lodash/debounce";
 
 const SharePage = () => {
   const { token } = useAuth();
@@ -23,10 +24,10 @@ const SharePage = () => {
   } = useQuery(GET_PUBLISHED_DOCUMENTS, {
     context: {
       headers: {
-        authorization: token ? `Bearer ${token}` : "", // 添加认证头
+        authorization: token ? `Bearer ${token}` : "",
       },
     },
-    skip: !token, // 如果没有 token 则跳过查询
+    skip: !token,
   });
 
   // 搜索文档
@@ -36,10 +37,10 @@ const SharePage = () => {
     error: errorSearch,
   } = useQuery(SEARCH_DOCUMENTS, {
     variables: { searchTerm },
-    skip: !searchTerm || !token, // 如果没有搜索词或 token 则跳过查询
+    skip: !searchTerm || !token,
     context: {
       headers: {
-        authorization: token ? `Bearer ${token}` : "", // 添加认证头
+        authorization: token ? `Bearer ${token}` : "",
       },
     },
   });
@@ -58,9 +59,12 @@ const SharePage = () => {
     }
   }, [searchData]);
 
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchTerm(e.target.value);
-  };
+  const handleSearchChange = useCallback(
+    debounce((value: string) => {
+      setSearchTerm(value);
+    }, 500),
+    []
+  );
 
   const showToast = (message: string) => {
     setToastMessage(message);
@@ -70,34 +74,41 @@ const SharePage = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header showToast={showToast} />
-      <div className="p-4">
-        <input
-          type="text"
-          placeholder="搜索文档..."
-          className="input input-bordered w-full mb-4"
-          value={searchTerm}
-          onChange={handleSearchChange}
-        />
-        {loading ? ( // 显示加载状态
-          <p>加载中...</p>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-            {loadingPublished && <p>加载中...</p>}
-            {errorPublished && <p>发生错误: {errorPublished.message}</p>}
-            {documents.map((doc) => (
-              <Link key={doc.id} href={`/read/${doc.id}`}>
-                <div className="border p-4 rounded-lg hover:shadow-lg transition-shadow">
-                  <h2 className="font-semibold text-lg">{doc.fileName}</h2>
-                  <p>
-                    {doc.content.length > 100
-                      ? `${doc.content.substring(0, 100)}...`
-                      : doc.content}
-                  </p>
-                </div>
-              </Link>
-            ))}
-          </div>
-        )}
+      <div className="container mx-auto p-4 flex-grow">
+        <div className="flex flex-col items-center mb-4 h-48 justify-center ">
+          <input
+            type="text"
+            placeholder="搜索文档..."
+            className="input input-bordered w-1/3 mb-4"
+            onChange={(e) => handleSearchChange(e.target.value)}
+          />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-4 min-h-[300px]">
+          {" "}
+          {/* 设置最小高度 */}
+          {loading ? (
+            <p>加载中...</p>
+          ) : (
+            <>
+              {loadingPublished && <p>加载中...</p>}
+              {errorPublished && <p>发生错误: {errorPublished.message}</p>}
+              {documents.map((doc) => (
+                <Link key={doc.id} href={`/read/${doc.id}`}>
+                  <div className="border p-4 rounded-lg hover:shadow-lg transition-shadow w-full h-full">
+                    {" "}
+                    {/* 设置宽度为100% */}
+                    <h2 className="font-semibold text-lg">{doc.fileName}</h2>
+                    <p>
+                      {doc.content.length > 500
+                        ? `${doc.content.substring(0, 500)}...`
+                        : doc.content}
+                    </p>
+                  </div>
+                </Link>
+              ))}
+            </>
+          )}
+        </div>
       </div>
       <Toast message={toastMessage} onClose={() => setToastMessage(null)} />
     </div>
